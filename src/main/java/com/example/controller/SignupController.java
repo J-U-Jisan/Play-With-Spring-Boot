c73c5f44c7eeef8f56901e4720ec7dc2d2160d5c
jisan <jalal.uddin@tigerit.com>
2024-05-19 18:51:57 +0600

    Data binding with signup form

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -1,10 +1,14 @@
 package com.example.controller;
 
 import com.example.application.service.UserApplicationService;
+import com.example.form.SignupForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,6 +17,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
 
     @Autowired
@@ -20,7 +25,8 @@ public class SignupController {
 
     /** Display the user signup screen */
     @GetMapping ("/signup")
-    public String getSignup(Model model, Locale locale) {
+    public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
+
         // Get gender
         Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
         model.addAttribute("genderMap", genderMap);
@@ -30,7 +36,14 @@ public class SignupController {
 
     /** User signup process */
     @PostMapping ("/signup")
-    public String postSignup() {
+    public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+
+        // Input check result
+        if (bindingResult.hasErrors()) {
+            return getSignup(model, locale, form);
+        }
+
+        log.info(form.toString());
         // Redirect to login screen
         return "redirect:/login";
     }
