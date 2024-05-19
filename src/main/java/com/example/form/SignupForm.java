281508d27a1a238f35f773c041662cde7031e846
Jalal Uddin Jisan <34156029+J-U-Jisan@users.noreply.github.com>
2024-05-20 00:10:24 +0600

    Validation Implementation

--- src/main/java/com/example/form/SignupForm.java
+++ src/main/java/com/example/form/SignupForm.java
@@ -1,17 +1,35 @@
 package com.example.form;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 public class SignupForm {
+
+    @NotBlank
+    @Email
     private String userId;
+
+    @NotBlank
+    @Length(min=4, max=100)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password;
+
+    @NotBlank
     private String userName;
+
     @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @NotNull
     private Date birthday;
+
+    @Min(20)
+    @Max(100)
     private Integer age;
+
+    @NotNull
     private Integer gender;
 }
