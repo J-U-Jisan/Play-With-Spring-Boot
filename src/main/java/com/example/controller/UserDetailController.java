2e2cbfb1588ecc0e62cd9305cd2ce5d823e3bead
Jalal Uddin Jisan <34156029+J-U-Jisan@users.noreply.github.com>
2024-06-01 11:25:03 +0600

    Implement transactional on update

--- src/main/java/com/example/controller/UserDetailController.java
+++ src/main/java/com/example/controller/UserDetailController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.domain.user.model.MUser;
 import com.example.domain.user.service.UserService;
 import com.example.form.UserDetailForm;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class UserDetailController {
 
     @Autowired
@@ -43,8 +45,12 @@ public class UserDetailController {
     /** User update process */
     @PostMapping(value ="/detail", params = "update")
     public String updateUser(UserDetailForm form, Model model) {
-        // Update user
-        userService.updateUserOne(form.getUserId(), form.getPassword(), form.getUserName());
+        try {
+            // Update user
+            userService.updateUserOne(form.getUserId(), form.getPassword(), form.getUserName());
+        } catch (Exception e) {
+            log.error("Error in user update", e);
+        }
 
         // Redirect to user list screen
         return "redirect:/user/list";
--- src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.domain.user.service.UserService;
 import com.example.repository.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -32,9 +33,14 @@ public class UserServiceImpl implements UserService {
         return mapper.findOne(userId);
     }
 
+    /** Update user */
+    @Transactional
     @Override
     public void updateUserOne(String userId, String password, String userName) {
         mapper.updateOne(userId, password, userName);
+
+        // Raise an exception
+//        int i = 1/0;
     }
 
     @Override
