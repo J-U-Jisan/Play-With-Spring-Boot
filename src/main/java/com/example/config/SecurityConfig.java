0ae5b063dc9223034f0b0a2b9c8c3ac354cc1f85
jisan <jalal.uddin@tigerit.com>
2024-07-28 17:52:15 +0600

    Added CSRF security

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -13,9 +13,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -58,8 +57,8 @@ public class SecurityConfig{
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
-                )
-                .csrf(AbstractHttpConfigurer::disable); // Disable CSRF
+                );
+//                .csrf(AbstractHttpConfigurer::disable); // Disable CSRF
 
         return http.build();
     }
